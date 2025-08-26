c1019391a1fc08550352b669a6c290ee164d020e
rosexalba <rosemaiellalv@gmail.com>
2025-08-26 12:02:44 -0700

    Operators

---
+++ JS-fundamentals-Part1/Project-03-Operators/script.js
@@ -0,0 +1,29 @@
+const Add = 5 + 3;
+console.log(Add);
+
+const multiply = 4 * 2;
+console.log(multiply);
+
+const compare = 10 > 7;
+console.log(compare);
+
+const subtract = 12 - 8;
+console.log(subtract);
+
+const divide = 20 / 4;
+console.log(divide);
+
+const remainder = 10 % 3;
+console.log(remainder);
+
+const check = 5 < 9;
+console.log(check);
+
+const equality = 10 == "10";
+console.log(equality);
+
+const isEqual = 5 === "5";
+console.log(isEqual);
+
+const notStrictly = 7 !== "7";
+console.log(notStrictly);
