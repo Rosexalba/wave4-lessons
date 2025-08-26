0270e39afe9e5b96e6a447abd2e0816d53a63b39
rosexalba <rosemaiellalv@gmail.com>
2025-08-26 11:15:55 -0700

    datatypes

---
+++ JS-fundamentals-Part1/Project-02-Data-Types/script.js
@@ -0,0 +1,14 @@
+const petName = "Zuly";
+console.log(petName, typeof petName);
+
+const heightInCm = 53;
+console.log(heightInCm, typeof heightInCm);
+
+const isStudent = true;
+console.log(isStudent, typeof isStudent);
+
+const lunchTime = null;
+console.log(lunchTime, typeof lunchTime);
+
+const favoriteFood = undefined;
+console.log(favoriteFood, typeof favoriteFood)
