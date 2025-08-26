9dc2d542df1fde9d31954245b05fe2b34fe7cf73
rosexalba <rosemaiellalv@gmail.com>
2025-08-26 10:42:21 -0700

    Variables

---
+++ JS-fundamentals-Part1/Project-01-variables/script.js
@@ -0,0 +1,17 @@
+const name = "Diego";
+console.log(name);
+
+let age = 22;
+console.log(age);
+
+const country = "Australia";
+console.log(country);
+
+let color = "Purple";
+console.log(color);
+
+const birthYear = 1998;
+console.log(birthYear);
+
+let hobby ="Coding";
+console.log(hobby)
