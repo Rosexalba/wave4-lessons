4e1932579d01ab960eca7dd654ccfcd0f918bb31
rosexalba <rosemaiellalv@gmail.com>
2025-08-29 11:41:39 -0700

    conditionals

---
+++ JS-Fundamentals-Part2/Project-05-Conditionals/script.js
@@ -0,0 +1,64 @@
+
+
+let score = 80;
+
+if (score > 80) {
+    console.log("Great Job!");
+};
+
+let age = 17;
+
+if (age < 18) {
+    console.log("You are a minor")
+}
+
+
+let isHungry = true;
+
+if (isHungry) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+let grade = 50;
+
+if (grade >= 90) {
+    console.log("you got an A")
+} else if (grade >= 80) {
+    console.log("you got a B")
+}
+  else if ( grade >= 70) {
+    console.log("you got a C")
+}
+  else if (grade >= 60) {
+    console.log("you got a D")
+} else {
+    console.log("you got an F");
+}
+
+
+let isLoggedIn = false; 
+
+if(isLoggedIn) {
+    console.log("True")
+} else {
+    console.log("False");
+}
+
+
+if (!isLoggedIn) {
+    console.log("Not logged in");
+}
+
+
+let tempertaure = 15;
+
+if (tempertaure > 10) {
+    console.log("true");
+}
+
+let temp = 15;
+
+console.log(temp > 30 ? "True" : "False");
