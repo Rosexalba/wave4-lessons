cff54bbfc2651ed0d438a81a051cc6971e36dd58
rosexalba <rosemaiellalv@gmail.com>
2025-08-29 12:49:06 -0700

    loops

---
+++ JS-fundamentals-Part3/Project-06-Loops/script.js
@@ -0,0 +1,27 @@
+for (let i = 1; i <=3; i++) {
+    console.log(i);
+}
+
+for (let i = 1; i<=5; i++) {
+    console.log("star " + i);
+}
+
+for (let i = 1; i <=5; i++) {
+    console.log("star");
+}
+
+
+
+const fruits = ["apple","banana", "orange"];
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+
+// for (let i = 2; i <= 10; i+= 2) {
+//     console.log(i);
+// }
+
+for (let i = 5; i >= 1; i-= 1) {
+    console.log(i);
+}
